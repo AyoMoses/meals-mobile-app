33ef445f510478e8ac186f2ac30c21342e3e70e8
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2020-02-28 22:39:06 +0100

    redux data and navigation options

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,18 +1,10 @@
-import React from "react";
-import {
-  View,
-  Text,
-  Button,
-  Image,
-  StyleSheet,
-  ScrollView
-} from "react-native";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
-import { MEALS } from "../data/dummy-data";
 import HeaderButton from "../components/HeaderButton";
 import DefaultText from "../components/DefaultText";
-import Colors from '../constants/Colors';
 
 const ListItem = props => {
   return (
@@ -23,9 +15,14 @@ const ListItem = props => {
 };
 
 const MealDetailScreen = props => {
+  const availableMeals = useSelector(state => state.meals.meals);
   const mealId = props.navigation.getParam("mealId");
 
-  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  const selectedMeal = availableMeals.find(meal => meal.id === mealId);
+
+  useEffect(() => {
+    props.navigation.setParams({ mealTitle: selectedMeal.title });
+  }, [selectedMeal]);
 
   return (
     <ScrollView>
@@ -50,9 +47,11 @@ const MealDetailScreen = props => {
 
 MealDetailScreen.navigationOptions = navigationData => {
   const mealId = navigationData.navigation.getParam("mealId");
-  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  const mealTitle = navigationData.navigation.getParam("mealTitle");
+  // const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
   return {
-    headerTitle: selectedMeal.title,
+    headerTitle: mealTitle,
     headerRight: (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
@@ -85,7 +84,7 @@ const styles = StyleSheet.create({
   listItem: {
     marginVertical: 10,
     marginHorizontal: 20,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderRadius: 5,
     borderWidth: 1,
     padding: 10
